e4e1616365972d460acd37097232a92a8a3e6ce5
Ian Wagner <ianthetechie@gmail.com>
2014-03-14 15:56:30 -0400

    COLORS!!! Also, a skeletal combat handling system for major events  (being noticed by a recognizer)

--- main.lisp
+++ main.lisp
@@ -26,10 +26,18 @@
   level)
 
 (defstruct game-state
-  fighting)
+  recognized)
+
+(defstruct adversary
+  type
+  hp
+  level)
+
+(defvar *player-color-pair* 1)
 
 (defvar *player* (make-player :hp 10 :max-hp 10 :level 1))
-(defvar *game-state* (make-game-state :fighting nil))
+(defvar *enemy*)
+(defvar *game-state* (make-game-state :recognized nil))
 (defvar *grid*)
 
 
@@ -39,6 +47,14 @@
      (cl-ncurses:initscr)
      (cl-ncurses:noecho)      ; Disable keyboard echo
      (cl-ncurses:curs-set 1)  ; Normal cursor visibility
+
+     ;; Set up color
+     (cl-ncurses:start-color)
+     (cl-ncurses:init-pair
+      *player-color-pair*
+      cl-ncurses:color_yellow
+      cl-ncurses:color_black)
+     
      ,@forms
      (cl-ncurses:endwin)))
 
@@ -64,18 +80,25 @@
                   #\*
                   #\Space))))))
 
-(defun draw-infobox ()
+(defun draw-string (row col str)
+  (loop
+     for c across str
+     for i from 0 to (length str)
+     do
+       (cl-ncurses:mvaddch
+        (floor row)
+        (floor (+ col i))
+        (char-code c))))
+
+(defun draw-infobox (title)
   (let* ((map-width (mapgen:grid-width *grid*))
              (map-height (mapgen:grid-height *grid*))
-             (width (/ map-width 2))
-             (height (/ map-height 4))
-             (row (- (/ map-height 2) (/ height 2)))
-             (col (- (/ map-width 2) (/ width 2))))
-        (draw-rect
-         (floor row)
-         (floor col)
-         (floor height)
-         (floor width))))
+             (width (floor (/ map-width 2)))
+             (height (floor (/ map-height 4)))
+             (row (floor (- (/ map-height 2) (/ height 2))))
+             (col (floor (- (/ map-width 2) (/ width 2)))))
+    (draw-rect row col height width)
+    (draw-string (+ row 2) (+ col 2) title)))
 
 (defun draw-map ()
   (mapgen:iterate-row-major
@@ -89,18 +112,20 @@
    (lambda (row))))
 
 (defun draw-player ()
+  (cl-ncurses:attron (cl-ncurses:color-pair *player-color-pair*))
   (cl-ncurses:mvaddch
    (player-row *player*)
    (player-col *player*)
-   (char-code #\@)))
+   (char-code #\@))
+  (cl-ncurses:attroff (cl-ncurses:color-pair *player-color-pair*)))
 
 (defun update-display ()
   (cl-ncurses:clear)
   (draw-map)
   (draw-player)
   (cl-ncurses:move (player-row *player*) (player-col *player*))
-  (if (game-state-fighting *game-state*)
-      (draw-infobox))
+  (if (game-state-recognized *game-state*)
+      (draw-infobox "You've been spotted by a recognizer!"))
   (cl-ncurses:refresh))
 
 
@@ -119,7 +144,7 @@
            (setf (player-col *player*) col))))
    (lambda (col))))
 
-(defun move-player (key)
+(defun do-player-action (key)
   (let ((row-delta 0)
         (col-delta 0))
     (cond
@@ -153,18 +178,27 @@
             (setf (player-row *player*) row)
             (setf (player-col *player*) col))))))
 
+(defun do-fight-action (key)
+  (cond
+    ((eql key #\r) (setf (game-state-recognized *game-state*) nil))))
 
 (defun main ()
   (format t "~%Generating a level...~%")
-  (setf *grid* (mapgen:generate-map 59 99))
+  (setf *grid* (mapgen:generate-map 75 271))
   (set-player-start)
   (with-curses-window
-    (let ((last-key nil))
+    (let ((last-key nil)
+          (state (make-random-state t)))
       (update-display)
       (loop while (not (eql #\Esc last-key)) do
            ;; Main game input loop
            (setf last-key (get-keyboard-char))
-           (move-player last-key)
+           (if (game-state-recognized *game-state*)
+               (do-fight-action last-key)
+               (progn
+                 (do-player-action last-key)
+                 (if (= (random 100 state) 0)
+                     (setf (game-state-recognized *game-state*) t))))
            (update-display)))))
 
 (main)
